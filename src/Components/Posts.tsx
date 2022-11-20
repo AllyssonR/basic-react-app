413d2841090544f878eadea488bf14e59537948b
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2022-11-20 15:35:59 -0300

    implement axios

--- src/Components/Posts.tsx
+++ src/Components/Posts.tsx
@@ -1,12 +1,30 @@
 import { Post } from "./Post";
-import { posts } from "../posts.json";
-import '../styles/posts.scss'
+import { api } from "../services/api";
+import "../styles/posts.scss";
+import { useEffect, useState } from "react";
+interface Postsprops {
+  title: string;
+  description: string;
+}
 export function Posts() {
+  const [posts, setPosts] = useState<Postsprops[]>([]);
+  useEffect(() => {
+    api
+      .get("/posts")
+      .then((response) => response.data)
+      .then((data) => setPosts(data));
+  }, []);
   return (
     <div>
       <ul>
         {posts.map((posts) => {
-          return <Post title={posts.title} paragraph={posts.description} />;
+          return (
+            <Post
+              key={posts.title}
+              title={posts.title}
+              paragraph={posts.description}
+            />
+          );
         })}
       </ul>
     </div>
